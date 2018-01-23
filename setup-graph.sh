ffef7fbebd095f3e83497b2a4f21dca46d4f09e4
FredLoney <loneyf@ohsu.edu>
2018-01-23 12:22:55 -0800

    Deprecate the -v password option in deference to the renamed -p option. Add the -f overwrite option. Simplify the usage text to conform to conventional Linux usage format. Add -h to the usage text. Move the usage warning after the usage syntax. Remove the unused _PROBLEMS count. Standardize the mixed indentation to 2 spaces. Check for a Linux platform. Guard against running install on a non-Linux platform. Issue an insecure warning if install does not supply the -n Neo4j password option. Only clone graph-importer if the current directory is not named graph-importer. Clone into a temp directory. Relax the -d target directory option to include or exclude a trailing /. Only chown on Linux, since Mac OS typically will not run Neo4j under a neo4j user. Only start and stop the Neo4j service on Linux.

--- setup-graph.sh
+++ setup-graph.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #-----------------------------------------------------------
-# Script for an initial setup and dataimport import of Reactome to the Graph
-# WARNING: do not execute as sudo, permission will be asked when required
+# Script to initial Neo4J and import Reactome MySQL data into
+# a graph database.
 #
 # 4 March 2015
 # Florian Korninger - fkorn@ebi.ac.uk
@@ -10,45 +10,53 @@
 #-----------------------------------------------------------
 
 usage="
-Script for an initial setup and dataimport import of Reactome to the Graph
-WARNING: Do not execute as sudo, permission will be asked when required
+Usage: $(basename "$0") [options]
 
-The password will only be updated after installing or updating the neo4j server
-The neo4j server can be updated without uninstalling it before.
-WARNING: If no password is specified the old password will persist.
-
-$(basename "$0") [-r <reactome_host> -s <reactome_port> —t <reactome_db_name> -u <reactome_db_user> -v <reactome_db_password> -d <neo4j_db_directory> -e <neo4j_db_name> -j <import_data> -i <install_neo4j> -m <neo4j_user> -n <neo4j_password> ]
-
-where:
+Options:
     -h  Program help/usage
-    -r  Reactome database host. DEFAULT: localhost
-    -s  Reactome database port. DEFAULT: 3306
-    -t  Reactome database name. DEFAULT: reactome
-    -u  Reactome database user. DEFAULT: reactome
-    -v  Reactome database password. DEFAULT: reactome
-    -d  Neo4j directory of Db. DEFAULT: /var/lib/neo4j/data/
-    -e  Neo4j name of graph Db. DEFAULT: graph.db
+    -i  Install Neo4j. DEFAULT: false
     -j  Import Reactome data. DEFAULT: false
-    -i  Install neo4j. DEFAULT: false
-    -n  Neo4j password (only set when neo4j is installed)."	
+    -f  Overwrite existing target database. DEFAULT: false
+    -r  Reactome MySQL database host. DEFAULT: localhost
+    -s  Reactome MySQL database port. DEFAULT: 3306
+    -t  Reactome MySQL database name. DEFAULT: reactome
+    -u  Reactome MySQL database user. DEFAULT: reactome
+    -p  Reactome MySQL database password. DEFAULT: reactome
+    -d  Neo4j target database parent directory. DEFAULT: /var/lib/neo4j/data/
+    -e  Neo4j target database name. DEFAULT: graph.db
+    -n  Neo4j password (ignored unless the -i Neo4j install option is enabled)
+
+WARNING: Do not execute as sudo, permission will be asked when required.
+
+Note: If the -i Neo4j install option is set, then the current stable Neo4j
+version will be installed. If the -n Neo4j password option is set, then the
+password will be updated after the Neo4j server is installed. The Neo4j server
+can be upgraded to a newer version without uninstalling it beforehand.
+Installation is only supported on Linux platforms.
+
+WARNING: If no password is specified in conjunction with the -i Neo4j
+install option, then the insecure default Neo4j password will remain
+unchanged.
+"
 
 _REACTOME_HOST="localhost"
 _REACTOME_PORT=3306
 _REACTOME_DATABASE="reactome"
 _REACTOME_USER="reactome"
 _REACTOME_PASSWORD="reactome"
-_GRAPH_DIR="/var/lib/neo4j/data/databases/"
+_GRAPH_PARENT_DIR="/var/lib/neo4j/data/databases"
 _GRAPH_NAME="graph.db"
+_OVERWRITE=false
 _IMPORT_DATA=false
 _INSTALL_NEO4J=false
 
-_PROBLEMS=0
+# Note: the 
 
 # :h (help) should be at the very end of the while loop
-while getopts ":r:s:t:u:v:d:e:m:n:ijh" option; do
+while getopts ":r:s:t:u:p:v:d:e:m:n:ijh" option; do
   case "$option" in
     h) echo "$usage"
-       exit
+       exit 0
        ;;
     r) _REACTOME_HOST=$OPTARG
        ;;
@@ -58,11 +66,17 @@ while getopts ":r:s:t:u:v:d:e:m:n:ijh" option; do
        ;;
     u) _REACTOME_USER=$OPTARG
        ;;
+    p) _REACTOME_PASSWORD=$OPTARG
+       ;;
     v) _REACTOME_PASSWORD=$OPTARG
-       ;; 
-    d) _GRAPH_DIR=$OPTARG
+       # TODO - Make this an error by 2019.
+       echo "Note: Option -v is deprecated. Please use -p instead."
+       ;;
+    d) _GRAPH_PARENT_DIR=$OPTARG
        ;;
     e) _GRAPH_NAME=$OPTARG
+       ;;
+    f) _OVERWRITE=true
        ;;
     i) _INSTALL_NEO4J=true
        ;;
@@ -76,106 +90,150 @@ while getopts ":r:s:t:u:v:d:e:m:n:ijh" option; do
        ;;
   esac
 done
-shift $((OPTIND - 1))
 
-# --- Check if the execution type has been passed --- #
+# There must be an install or import execution type option.
 if ! $_INSTALL_NEO4J && ! $_IMPORT_DATA ; then
- echo "missing argument execution type -i|-j"
+ echo "Missing argument execution type -i or -j. Usage:"
     echo "$usage"
     exit 1
 fi;
 
-if ${_INSTALL_NEO4J} = true; then
-    echo "start installing neo4j"
-    sudo sh -c "wget -O - https://debian.neo4j.org/neotechnology.gpg.key | sudo apt-key add -" >/dev/null 2>&1
-    sudo sh -c "echo 'deb http://debian.neo4j.org/repo stable/' >/tmp/neo4j.list" >/dev/null 2>&1
-    sudo mv /tmp/neo4j.list /etc/apt/sources.list.d
-    sudo apt-get update
-    sudo apt-get install neo4j
-    echo "installing neo4j finished"
-    if [ ! -z "$_NEO4J_PASSWORD" ]; then 
-	echo "removing old authentication"
-	if sudo service neo4j status >/dev/null 2>&1; then
-            echo "Shutting down Neo4j DB"
-            if ! sudo service neo4j stop >/dev/null 2>&1; then
-            	echo "an error occurred while trying to shut down neo4j db"
-		exit 1
-    	    fi
-	fi
-	sudo rm /var/lib/neo4j/data/dbms/auth
-	if ! sudo service neo4j status >/dev/null 2>&1; then
-	    if ! sudo service neo4j start >/dev/null 2>&1; then
-		echo "An error occurred while trying to start neo4j"
-		exit 1
-	    fi
-	fi
-	echo "setting new password for user neo4j"	
-	curl -H "Content-Type: application/json" -X POST -d '{"password":"'${_NEO4J_PASSWORD}'"}' -u neo4j:neo4j http://localhost:7474/user/neo4j/password >/dev/null >/dev/null 2>&1
-    fi 
+# Are we running on Linux?
+_PLATFORM=`uname -s`
+case "$_PLATFORM" in
+  Linux|CYGWIN*)
+     # CygWin is close enough
+     _IS_LINUX=true
+     ;;
+  *)
+     _IS_LINUX=false
+     ;;
+esac
+
+if $_INSTALL_NEO4J ; then
+  if ! $_IS_LINUX ; then
+    echo "Neo4j installation on non-Linux platform '$_PLATFORM' is not supported by this script."
+    echo "Consult http://neo4j.com/docs/operations-manual/current/installation for Neo4j"
+    echo "installation instructions on this platform."
+    exit 1
+  fi
+  echo "Starting Neo4j installion..."
+  sudo sh -c "wget -O - https://debian.neo4j.org/neotechnology.gpg.key | sudo apt-key add -" >/dev/null 2>&1
+  sudo sh -c "echo 'deb http://debian.neo4j.org/repo stable/' >/tmp/neo4j.list" >/dev/null 2>&1
+  sudo mv /tmp/neo4j.list /etc/apt/sources.list.d
+  sudo apt-get update
+  sudo apt-get install neo4j
+  echo "Neo4j installation finished."
+  if [ -z "$_NEO4J_PASSWORD" ]; then
+    echo "WARNING: the new Neo4j installation password is insecure and should be reset."
+  else
+    echo "Removing old authentication..."
+    if sudo service neo4j status >/dev/null 2>&1; then
+      echo "Shutting down Neo4j DB..."
+      if ! sudo service neo4j stop >/dev/null 2>&1; then
+        echo "An error occurred while trying to shut down Neo4."
+        exit 1
+      fi
+    fi
+    sudo rm /var/lib/neo4j/data/dbms/auth
+    if ! sudo service neo4j start >/dev/null 2>&1; then
+      echo "An error occurred while trying to start Neo4j."
+      exit 1
+    fi
+    echo "Setting new password for user Neo4j..."
+    curl -H "Content-Type: application/json" -X POST -d '{"password":"'${_NEO4J_PASSWORD}'"}' -u neo4j:neo4j http://localhost:7474/user/neo4j/password >/dev/null >/dev/null 2>&1
+  fi
 fi
 
-if ${_IMPORT_DATA} = true; then
-    echo "Checking if current directory is valid project"
-    if ! mvn -q clean package -DskipTests; then
-        if [ ! -f /target/BatchImporter-jar-with-dependencies.jar ]; then
-            echo "Cloning new repo from git"
-            git clone https://github.com/reactome/graph-importer.git
-            _PATH="/graph-importer"
-
-            echo "Started packaging reactome project"
-            if ! mvn -q -f .${_PATH}/pom.xml clean package -DskipTests; then
-                echo "An error occurred when packaging the project"
-                exit 1
-            fi
-        fi
+if $_IMPORT_DATA ; then
+  if $_IS_LINUX ; then
+    if sudo service neo4j status; then
+      echo "Shutting down Neo4j in order to prepare data import..."
+      if ! sudo service neo4j stop; then
+        echo "An error occurred while trying to shut down Neo4j."
+      exit 1
+      fi
     fi
+  fi
 
-    if [ "$_GRAPH_DIR" == "/var/lib/neo4j/data/" ]; then
-        if sudo service neo4j status; then
-            echo "Shutting down Neo4j DB in order to prepare dataimport import"
-            if ! sudo service neo4j stop; then
-                echo "An error occurred while trying to shut down neo4j db"
-            exit 1
-            fi
-        fi
+  # If the current working directory is not named graph-importer,
+  # then clone the graph-importer into a temp directory.
+  if [ "${PWD##*/}" == "graph-importer" ]; then
+    _PROJECT_DIR="$PWD"
+  else
+    _TEMPDIR=`mktemp -d`
+    _PROJECT_DIR="$_TEMPDIR"
+    _REPO="https://github.com/reactome/graph-importer.git"
+    echo "Cloning $_REPO into ${_TEMPDIR}..."
+    cp -R $HOME/workspace/reactome/server/graph-importer/* $_PROJECT_DIR
+    #git clone "$_REPO" "$_TEMPDIR"
+  fi
+  _JAR_FILE="$_PROJECT_DIR/target/BatchImporter.jar"
+  if [ ! -f "$_JAR_FILE" ]; then
+    echo "Packaging the reactome graph-importer project..."
+    if ! (cd "$_PROJECT_DIR"; mvn -q clean package -U -DskipTests); then
+      echo "An error occurred packaging the project."
+      exit 1
     fi
-
-    if [ ! -d ${_GRAPH_DIR}${_GRAPH_NAME} ]; then
-        echo "Creating new database folder"
-        if ! sudo mkdir ${_GRAPH_DIR}${_GRAPH_NAME}; then
-            echo "An error occurred while trying to create a new database folder"
-            exit 1
-        fi
+    if [ ! -f "$_JAR_FILE" ]; then
+      echo "The jar file was not found after the Maven build: $_JAR_FILE"
+      exit 1
     fi
+  fi
+  echo "Importer jar file: $_JAR_FILE"
 
-    echo "Changing permissions of neo4j graph"
-    if ! sudo chown -R ${USER} ${_GRAPH_DIR}${_GRAPH_NAME}; then
-        echo "An error occurred when trying to change permissions of the neo4j graph"
+  # Remove trailing /
+  _GRAPH_PARENT_DIR="${_GRAPH_PARENT_DIR%/}"
+  # The parent directory must exist.
+  if [ ! -d "$_GRAPH_PARENT_DIR" ]; then
+    echo "Target graph parent folder must exist: $_GRAPH_PARENT_DIR"
+    exit 1
+  fi
+  
+  _GRAPH_DIR="${_GRAPH_PARENT_DIR}/${_GRAPH_NAME}"
+  # Make the target graph directory, if necessary.
+  if [ -d "$_GRAPH_DIR" ]; then
+    if ! $_OVERWRITE ; then
+      echo "Target graph database already exists: $GRAPH_DIR"
+      exit 1
     fi
+  elif ! mkdir "$_GRAPH_DIR"; then
+    echo "Creating new database folder..."
+    echo "Could not create the target graph database folder: $GRAPH_DIR"
+    exit 1
+  fi
 
-    echo "Started importing data to the neo4j database"
-    if ! java -jar .${_PATH}/target/BatchImporter-jar-with-dependencies.jar -h ${_REACTOME_HOST} -s ${_REACTOME_PORT} -d ${_REACTOME_DATABASE} -u ${_REACTOME_USER} -p ${_REACTOME_PASSWORD} -n ${_GRAPH_DIR}${_GRAPH_NAME}; then
-        echo "An error occurred during the data import process"
-        exit 1
+  if $_IS_LINUX; then
+    echo "Changing owner of the target Neo4j graph folder to ${USER}..."
+    if ! sudo chown -R ${USER} ${_GRAPH_DIR}; then
+      echo "An error occurred when trying to change owner of the target Neo4j graph folder."
+      exit 1
     fi
-    echo "DataImport finished successfully!"
+  fi
 
-    echo "Changing permissions of neo4j graph"
-    if ! sudo chown -R neo4j ${_GRAPH_DIR}${_GRAPH_NAME}; then
-        echo "An error occurred when trying to change permissions of the neo4j graph"
-        exit 1
+  echo "Importing data to Neo4j..."
+  if ! java -jar ${_JAR_FILE} -h ${_REACTOME_HOST} -s ${_REACTOME_PORT} -d ${_REACTOME_DATABASE} -u ${_REACTOME_USER} -p ${_REACTOME_PASSWORD} -o ${_GRAPH_DIR}; then
+    echo "An error occurred during the data import process."
+    exit 1
+  fi
+  echo "Data successfully imported into $_GRAPH_DIR."
+
+  if $_IS_LINUX; then
+    echo "Changing owner of Neo4j graph to user neo4j..."
+    if ! sudo chown -R neo4j ${_GRAPH_DIR}; then
+      echo "An error occurred when trying to change owner of the Neo4j graph."
+      exit 1
     fi
-fi
-
-if ! sudo service neo4j status >/dev/null 2>&1; then
-    echo "Starting neo4j database"
+    echo "Starting Neo4j database..."
     if ! sudo service neo4j start; then
-        echo "Neo4j database could not be started"
-        exit 1
+      echo "Neo4j database could not be started."
+      exit 1
     fi
+  fi
+  if [ -n "$_TEMPDIR" ]; then
+    rm -r "$_TEMPDIR"
+  fi
 fi
 
-if [ ! -z "$_PATH" ]; then
-    sudo rm -R graph-importer
-fi
-echo "Script finished with"  ${_PROBLEMS} "problems!"
+echo `basename $0` "execution successful."
+exit 0
